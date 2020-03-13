411ceeaa2ae6ae548a068e6cf22d385ce0aa1b13
sdfasd <1242447527@qq.com>
2020-03-14 00:19:01 +0800

    ss

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-
-let   data=[
-  {id:0,name:'lisi',age:23}
-  ]
-console.log(data);
+data:[
+  {id:0,name:'lisi',age:30,url:'http://www.baidu.com'},
+  {id:1,name:'lisi',age:25,url:'http://www.baidu.com'},
+  {id:2,name:'lisi',age:30,url:'http://www.baidu.com'},
+  {id:3,name:'lisi',age:30,url:'http://www.baidu.com'},
+]
